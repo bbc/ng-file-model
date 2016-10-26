1a211a9543f537d3f2a218eae37c609f2df01210
Phill Barron <phillipbarron242@users.noreply.github.com>
2016-10-26 10:07:03 +0100

    Update ng-file-model.js

--- ng-file-model.js
+++ ng-file-model.js
@@ -8,19 +8,19 @@
             },
             link: function (scope, element, attributes) {
                 element.bind("change", function (changeEvent) {
-                    var reader = new FileReader();
-                    reader.onload = function (loadEvent) {
+                    //var reader = new FileReader();
+                    //reader.onload = function (loadEvent) {
                         scope.$apply(function () {
                             scope.ngFileModel = {
                                 lastModified: changeEvent.target.files[0].lastModified,
                                 lastModifiedDate: changeEvent.target.files[0].lastModifiedDate,
                                 name: changeEvent.target.files[0].name,
                                 size: changeEvent.target.files[0].size,
-                                type: changeEvent.target.files[0].type,
-                                data: loadEvent.target.result
+                                type: changeEvent.target.files[0].type
+                                //data: loadEvent.target.result
                             };
                         });
-                    }
+                    //}
                     //reader.readAsDataURL(changeEvent.target.files[0]);
                 });
             }
